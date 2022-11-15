a5ef4d0febf675e2d25581b36f00feed02ac30fb
TalhaShakel <shakeeltalha637@gmail.com>
2022-11-15 18:40:48 +0500

    Started

--- lib/screens/Home/Live/live_streaming.dart
+++ lib/screens/Home/Live/live_streaming.dart
@@ -19,67 +19,129 @@ class Live_Streaming extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.all(15.0.sp),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Vx.hexToColor("#5468FD"),
-                          borderRadius: BorderRadius.all(Radius.circular(25))),
-                      child: Row(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Vx.hexToColor("#5468FD"),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        child: Row(
+                          children: [
+                            2.widthBox,
+                            Image.asset("assets/img2.png"),
+                            2.widthBox,
+                            "John Doe".text.white.make(),
+                            2.widthBox,
+                            Icon(
+                              Icons.add_circle_outline,
+                              color: Vx.white,
+                            ),
+                            2.widthBox,
+                          ],
+                        ),
+                      ),
+                      70.widthBox,
+                      "Live"
+                          .text
+                          .white
+                          .make()
+                          .box
+                          .padding(EdgeInsets.all(3))
+                          .color(Color.fromRGBO(235, 87, 87, 1))
+                          .bottomRounded(value: 3.sp)
+                          .topRounded(value: 3.sp)
+                          .make()
+                          .objectTopLeft(),
+                      3.widthBox,
+                      Row(
                         children: [
-                          2.widthBox,
-                          Image.asset("assets/img2.png"),
-                          2.widthBox,
-                          "John Doe".text.white.make(),
-                          2.widthBox,
                           Icon(
-                            Icons.add_circle_outline,
+                            Icons.remove_red_eye,
+                            color: Vx.white,
+                            size: 15,
+                          ),
+                          "8.1k".text.white.make()
+                        ],
+                      )
+                          .box
+                          .padding(EdgeInsets.all(3))
+                          .color(Color.fromRGBO(0, 0, 0, 0.28))
+                          .bottomRounded(value: 3.sp)
+                          .topRounded(value: 3.sp)
+                          .make(),
+                      3.widthBox,
+                      Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(color: Vx.white)),
+                          child: Icon(Icons.close, color: Vx.white))
+                    ],
+                  ),
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: Column(
+                        children: [
+                          200.sp.heightBox,
+                          Image.asset(
+                            "assets/Icon.png",
                             color: Vx.white,
                           ),
-                          2.widthBox,
+                          "150".text.white.make(),
+                          33.sp.heightBox,
+                          GestureDetector(
+                            onTap: () {
+                              
+                            },
+                            child: Icon(
+                              Icons.comment,
+                              color: Vx.white,
+                            ),
+                          ),
+                          "150".text.white.make(),
+                          33.sp.heightBox,
+                          Icon(
+                            Icons.share,
+                            color: Vx.white,
+                          ),
+                          "150".text.white.make(),
                         ],
-                      ),
-                    ),
-                    70.widthBox,
-                    "Live"
-                        .text
-                        .white
-                        .make()
-                        .box
-                        .padding(EdgeInsets.all(3))
-                        .color(Color.fromRGBO(235, 87, 87, 1))
-                        .bottomRounded(value: 3.sp)
-                        .topRounded(value: 3.sp)
-                        .make()
-                        .objectTopLeft(),3.widthBox,
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.remove_red_eye,
-                          color: Vx.white,
-                          size: 15,
-                        ),
-                        "8.1k".text.white.make()
-                      ],
-                    )
-                        .box
-                        .padding(EdgeInsets.all(3))
-                        .color(Color.fromRGBO(0, 0, 0, 0.28))
-                        .bottomRounded(value: 3.sp)
-                        .topRounded(value: 3.sp)
-                        .make(),3.widthBox,
-                    Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(color: Vx.white)),
-                        child: Icon(Icons.close, color: Vx.white))
-                  ],
-                ),
-Icon(Icons.heart_broken),
-                Image.asset("assets/Icon.png",color: Vx.white,)
-              ],
+                      )),
+                  ListTile(
+                    leading: Image.asset("assets/img2.png"),
+                    title: "Jhon Doe".text.white.bold.make(),
+                    subtitle:
+                        "Lorem ipsum dolor sit amet, consectetur adi piscing elit.  Quisque interdum blandit ipsum sed scelerisque."
+                            .text
+                            .size(12)
+                            .white
+                            .make(),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/img2.png"),
+                    title: "Jhon Doe".text.white.bold.make(),
+                    subtitle:
+                        "Lorem ipsum dolor sit amet, consectetur adi piscing elit.  Quisque interdum blandit ipsum sed scelerisque."
+                            .text
+                            .size(12)
+                            .white
+                            .make(),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/img2.png"),
+                    title: "Jhon Doe".text.white.bold.make(),
+                    subtitle:
+                        "Lorem ipsum dolor sit amet, consectetur adi piscing elit.  Quisque interdum blandit ipsum sed scelerisque."
+                            .text
+                            .size(12)
+                            .white
+                            .make(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
